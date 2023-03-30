1fa355b043a1af302036373fba25d52c26e693ee
Juan Diego Mejía Maestre <mejiamaestrejuandiego@gmail.com>
2023-03-30 16:09:10 -0500

    definir modelos

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'user_id',
+        'created_at',
+    ];
+
+
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+    ];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'name',
+        'last_name',
+        'email',
+        'password',
+        'birth_day',
+        'role_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'birth_day' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+            ];
 }
